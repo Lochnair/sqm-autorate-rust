34b3fc0850881a084e4ac80ad1dd9b27ed6c091c
Nils Andreas Svee <me@lochnair.net>
2022-10-15 13:49:05 +0200

    Add explanation to endian module

--- src/endian.rs
+++ src/endian.rs
@@ -2,6 +2,11 @@ pub trait ToNativeEndian {
     fn to_ne(self) -> Self;
 }
 
+/**
+ * A simple trait that adds a function to u32/u64 types
+ * to convert a number's endianness to native-endian
+ * based on the platform the code was built for
+ */
 impl ToNativeEndian for u32 {
     #[cfg(target_endian = "big")]
     fn to_ne(self) -> Self {
